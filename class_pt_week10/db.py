8d15eb43c9126e1a86450383998431d92ef4bb58
SonSophakOtra <102602021+SonSophakOtra@users.noreply.github.com>
2022-04-21 21:49:26 +0700

    Add files via upload

---
+++ class_pt_week10/db.py
@@ -0,0 +1,66 @@
+'''
+2022247_sonsophakotra
+2022170_Sokornnika
+'''
+
+
+
+
+
+from turtle import update
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="cat_db",
+    port="3306"
+)
+
+
+cursor = mydb.cursor()
+
+
+def register_cat(cat_info):
+
+    sql = "INSERT INTO cats (name, gender, breed, dob, description) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(sql, cat_info)
+    mydb.commit()
+
+    print("Registration completed!\n")
+
+
+
+def get_cats():
+
+    sql = "SELECT * from cats"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
+
+
+def get_cat(id):
+    sql = f"SELECT * FROM cats WHERE id={id}"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return result
+
+
+def update_cat(cat_info):
+    id, name, gender, breed, dob, description = cat_info
+    sql = f"UPDATE cats SET name='{name}', gender='{gender}', breed='{breed}', dob='{dob}', description='{description}' WHERE id={id}"
+    cursor.execute(sql)
+    mydb.commit()
+
+    print("Update completed!\n")
+
+
+def remove_cat(id):
+    sql = f"DELETE FROM cats WHERE id={id}"
+    cursor.execute(sql)
+    mydb.commit()
+    print("Remove completed!\n")
+
